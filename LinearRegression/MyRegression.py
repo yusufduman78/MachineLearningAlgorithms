5d31787395fdeb61a1b83e6843ba2ef8e31b45e3
smoke7806 <yusuf78duman@gmail.com>
2025-07-13 17:19:04 +0300

    for memory and time efficient added fitting with normal equation and auto or manual control for gradient or normal equation choosing

--- LinearRegression/MyRegression.py
+++ LinearRegression/MyRegression.py
@@ -74,35 +74,46 @@ class MyPolynomialFeatures:
 
 
 class MyLinearRegression:
-    def __init__(self, learning_rate=0.0005, epochs=100000, degree=1):
+    def __init__(self, learning_rate=0.0005, epochs=100000, threshold=1000, method='auto'):
         self.learning_rate = learning_rate
         self.epochs = epochs
+        self.threshold = threshold
+        self.method = method
         self.cost_history = []
-        self.degree = degree
 
+    def fit(self, X, y, epochs=None, learning_rate=None):
 
-    def fit(self, X, y, epochs=None, learning_rate=None, degree=None):
+        # if method is 'normal' threshold is not taken into account
+        if self.method == 'normal' or (self.method == 'auto' and X.shape[0] < self.threshold):
+            self.fit_with_normal_equation(X, y)
+        else:
+            self.fit_with_gradient_descent(X, y, epochs, learning_rate)
+
+    def fit_with_normal_equation(self,X,y):
+        if np.allclose(X[:,0], 1):
+            self.w = np.linalg.pinv(X.T @ X) @ X.T @ y
+            self.b = 0
+        else:
+            X_with_bias = np.hstack((np.ones((X.shape[0], 1)), X))
+            w_now = np.linalg.pinv(X_with_bias.T @ X_with_bias) @ X_with_bias @ y
+            self.b = w_now[0]
+            self.w = w_now[1:]
+        self.cost_history = [self.compute_cost(X, y)]
+
+    def fit_with_gradient_descent(self,X,y,epochs = None,learning_rate = None):
         if epochs is None:
             epochs = self.epochs
         if learning_rate is None:
             learning_rate = self.learning_rate
-        if degree is not None:
-            self.degree = degree
-
-        if self.degree > 1:
-            X = self.generate_polynomial_features(X, self.degree)
-
         self.w = np.zeros(X.shape[1])
         self.b = 0
-        self.gradient_descent(X, y, epochs, learning_rate)
+        self.gradient_descent(X,y,epochs, learning_rate)
 
-    def predict(self, X, is_training=False):
-        if self.degree > 1 and not is_training:
-            X = self.generate_polynomial_features(X, self.degree)
+    def predict(self, X):
         return np.dot(X, self.w) + self.b
 
     def compute_cost(self, X, y):
-        y_pred = self.predict(X, is_training = True)
+        y_pred = self.predict(X)
         m = X.shape[0]
         total_cost = np.sum((y_pred - y)**2) / (2 * m)
         return total_cost
@@ -126,7 +137,7 @@ class MyLinearRegression:
     def gradient_descent(self, X, y, iters, learning_rate):
         self.cost_history = []
         for i in range(iters):
-            y_pred = self.predict(X, is_training=True)
+            y_pred = self.predict(X)
             dj_dw, dj_db = self.compute_gradient(X, y, y_pred)
             self.w = self.w - learning_rate * dj_dw
             self.b = self.b - learning_rate * dj_db
@@ -134,6 +145,7 @@ class MyLinearRegression:
                 cost = self.compute_cost(X, y)
                 self.cost_history.append(cost)
                 print(f"Epoch {i}: Cost = {cost}")
+
 
     def show_cost_graphic(self):
         plt.plot(self.cost_history)
@@ -159,33 +171,39 @@ class MyLinearRegression:
 
         return {"mse": mse, "rmse": rmse, "mae": mae, "r2": r2}
 
-    def generate_polynomial_features(self, X, degree):
-        n_features = X.shape[1]
-        new_columns = []
-        for d in range(1, degree + 1):
-            combs = list(combinations_with_replacement(range(n_features), d))
-            for comb in combs:
-                new_column = np.prod(X[:, comb], axis=1)
-                new_columns.append(new_column.reshape(-1, 1))
-        return np.hstack(new_columns)
 
-split_index = int(len(X) * 0.8)
+np.random.seed(42)  # Tekrarlanabilirlik için
 
-# Veriyi ölçeklendirme (standartlaştırma)
+n_samples = 50
+X = np.random.uniform(10, 30, size=(n_samples, 3))
+
+# Gerçek fonksiyon: y = 3*X1 + 2*X2^2 - X3 + gürültü
+y = 3*X[:,0] + 2*(X[:,1]**2) - X[:,2] + np.random.normal(0, 5, size=n_samples)
+
+# Veri bölme
+split_index = int(0.8 * n_samples)
+X_train, X_test = X[:split_index], X[split_index:]
+y_train, y_test = y[:split_index], y[split_index:]
+
+# Ölçeklendirme
 scaler = Scaler()
-X_scaled = scaler.fit_transform(X)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-X_train = X_scaled[:split_index]
-X_test = X_scaled[split_index:]
-y_train = y[:split_index]
-y_test = y[split_index:]
+poly = MyPolynomialFeatures(degree=2, bias_including=True)
+X_train_poly = poly.fit_transform(X_train_scaled)
+X_test_poly = poly.transform(X_test_scaled)
+
+model = MyLinearRegression(learning_rate=0.001, epochs=50000)
+model.fit(X_train_poly, y_train)
+
+y_pred_train = model.predict(X_train_poly)
+y_pred_test = model.predict(X_test_poly)
+
+print("Eğitim seti performansı:")
+model.evaluate(X_train_poly, y_train)
+print("\nTest seti performansı:")
+model.evaluate(X_test_poly, y_test)
 
 
-model = MyLinearRegression(degree=2)
 
-model.fit(X_train,y_train)
-
-y_pred = model.predict(X_test)
-
-print("predict: ",y_pred)
-print("\n real value", y_test)
