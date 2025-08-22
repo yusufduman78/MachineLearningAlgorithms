440bda2a1b898a560130fdf90eb201bc51d6162e
smoke7806 <yusuf78duman@gmail.com>
2025-08-22 18:42:43 +0300

    CNN model building

--- Convolutional Neural Network/CNN.py
+++ Convolutional Neural Network/CNN.py
@@ -56,10 +56,31 @@ visualize(trainloader,4)
 
 # %% model building and loss,optimizer
 
-
-
-
-
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3,out_channels=32,kernel_size=3,padding=1) # in_channels = rgb, out channels = filter numbers, 32x32
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.dropout1 = nn.Dropout2d(0.2)
+        self.conv3 = nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.dropout2 = nn.Dropout(0.4)
+        self.fc1 = nn.Linear(128*4*4,128)
+        self.fc2 = nn.Linear(128,72)
+        self.fc3 = nn.Linear(72,10)
+    def forward(self,x):
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.dropout1(self.pool(self.relu(self.bn2(self.conv2(x)))))
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = torch.flatten(x,1)
+        x = self.dropout2(self.relu(self.fc1(x)))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 # %% train
 
